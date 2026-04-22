24e467f47371e839a0780e913c7eea25972a9ed1
GabeRivera97 <gabriel.j.rivera97@gmail.com>
2026-04-21 21:49:55 -0400

    Major refactor: backend structure, reports, UI improvements. Hopefully made all the buttons work, including reports. Only thing missing is the ability to add another project

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
-
+import webbrowser
+from threading import Timer
+import os
 from app import create_app
 
 app = create_app()
 
+def open_browser():
+    webbrowser.open_new("http://127.0.0.1:5000/")
+
 if __name__ == "__main__":
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        Timer(1, open_browser).start()
     app.run(debug=True)
